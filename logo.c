e2aa5a24e38998caf755d63754103af06fd0143c
amzy-0 <azymiamin@gmail.com>
2022-05-14 01:41:59 +0430

    version added

--- logo.c
+++ logo.c
@@ -50,5 +50,16 @@ void logo(){
     print_on_terminal("                  %%%%%%%%%%%%%%                ");
     newline_on_terminal();
 
+    newline_on_terminal();
 
+    newline_on_terminal();
+
+
+    print_on_terminal("|------------------|");
+    newline_on_terminal();
+    print_on_terminal("!Welcome to charleX!");
+    newline_on_terminal();
+    print_on_terminal("!    version 0.1   !");
+    newline_on_terminal();
+    print_on_terminal("|------------------|");
 }
